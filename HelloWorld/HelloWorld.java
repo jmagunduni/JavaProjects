3e7c4e0a92aeaa36491144f769e879660ef862f5
joshuamagunduni <jmagunduni@gmail.com>
2025-01-12 20:56:15 +0200

    Initial commit

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,5 +2,9 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        boolean isAlien = false;
+        if (isAlien == true) {
+            System.out.println("Alien");
+        }
     }
 }
